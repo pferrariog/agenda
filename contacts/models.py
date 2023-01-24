0e367eed96733da44fc4b87d3a77c049b83be6e8
pferrariog <pdrferrari08@gmail.com>
2023-01-23 22:18:23 -0300

    feat: profile photo

--- contacts/models.py
+++ contacts/models.py
@@ -18,6 +18,7 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     show = models.BooleanField(default=True)
+    photo = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self):
         return self.name
